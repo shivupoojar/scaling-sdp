e6f0c6c35a0df407f9ecdaa4c5e862074c155d97
shivupoojar <shivu.poojar@gmail.com>
2022-11-30 08:07:40 +0200

    added code for deintify

--- healthcare/deindify/handler.py
+++ healthcare/deindify/handler.py
@@ -1,7 +1,11 @@
+import json
 def handle(req):
-    """handle a request to the function
-    Args:
-        req (str): request body
-    """
-
-    return req
+    di_data = {}
+    raw_data = json.loads(req)
+    di_data['timestamp']=raw_data['timestamp']
+    di_data['device_id']=raw_data['device_id']
+    di_data['patient_id']=raw_data['patient_id']
+    di_data['temperature']=raw_data['temperature']
+    di_data['systolic']=raw_data['systolic']
+    di_data['"diastolic']=raw_data['"diastolic']
+    return josn.dumps(di_data)
